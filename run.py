6871d0ec869bcb9107a80366f8f95ac8699e27ac
William YT Hu <halomasterpizza@gmail.com>
2023-12-07 14:17:35 -0800

    Added argparse support in run.py, Added a load_data.ipynb to help set up dataset (for reproducibility and stuff)

--- run.py
+++ run.py
@@ -1,5 +1,30 @@
 import steelthread
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_size", type = int, required= True, help="Number of rows to take from dataset")
+parser.add_argument("--random_seed", type = int, required= True, help="Seed to randomly shuffle data")
+parser.add_argument("--loadEmbed", type = bool, required= True, help= "Whether to create the embeddings or not")
+parser.add_argument("--loadTF", type = bool, required= True, help= "Whether to create the inverted index or not")
+parser.add_argument("--queryMethod", type = str, required= True, help= "Which method for the search engine to query.\
+                    You can choose from: query_results_lc_naive_custom, query_results_tfidf, query_results_BM25, \
+                    query_results_embed, query_results_lc_norm, query_results_odds_evens, query_results_faiss_kw \
+                    query_results_faiss_cos")
+parser.add_argument("--tfMethod", type = str, required= True, help= "Which method for the search engine to query.\
+                    You can choose from: BM25 or TFIDF")
+parser.add_argument("--tf_alpha", type = float, required= True, help= "Float value setting the Linear Combination Weight")
+parser.add_argument("--bigrams", type = bool, required= True, help= "Boolean determining bigram support or not")
+parser.add_argument("--res_per_query", type = int, required= True, help= "Int determining the k amount of top results to return per query.")
+
+args = vars(parser.parse_args())
+
+# Run this for example:
+# python run.py --data_size 100000 --random_seed 1 --loadEmbed True --loadTF True --queryMethod "query_results_lc_naive_custom" --tfMethod "BM25" --tf_alpha 0.75 --bigrams True --res_per_query 50
 
 if __name__ == '__main__':
-    model = steelthread.create_model(100000, 1, True, False, "query_results_lc_naive_custom", "BM25", 0.75, True)
-    steelthread.create_results(model, 50)
+    # print(args)
+    model = steelthread.create_model(args["data_size"], args["random_seed"], args["loadEmbed"], 
+                                     args["loadTF"], args["queryMethod"], args["tfMethod"], args["tf_alpha"]
+                                     , args["bigrams"])
+    steelthread.create_results(model, args["res_per_query"])
+
